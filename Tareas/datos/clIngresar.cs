269cf3da23563db2bcd141aa2c4d7021891fe60f
Zebzki <teamsebas88@gmail.com>
2024-11-09 13:00:33 -0500

    funcionalidad del login hecha

---
+++ Tareas/datos/clIngresar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Tareas.entidades;
+
+namespace Tareas.datos
+{
+    public class clIngresar
+    {
+        public clUsuario mtdIngrsar(clUsuario oData)
+        {
+            clConexion con = new clConexion();
+            clUsuario oUsuario = null;
+            SqlConnection conexion = con.mtdAbrirConexion();
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("spLogin", conexion))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@email", oData.email);
+                    cmd.Parameters.AddWithValue("@password", oData.contraseña);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            oUsuario = new clUsuario()
+                            {
+                                idUsuario = reader.GetInt32(reader.GetOrdinal("idUsuario")),
+                                nombre = reader.GetString(reader.GetOrdinal("nombre")),
+                                apellido = reader.GetString(reader.GetOrdinal("apellido")),
+                                email = reader.GetString(reader.GetOrdinal("email")),
+                                contraseña = reader.GetString(reader.GetOrdinal("contraseña"))
+                            };
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                con.mtdCerrarConexion();
+            }
+            return oUsuario;
+
+        }
+    }
+}
